7efc5171f4fa84d6e8ada7e29f6a3290d893fdcc
Pab10Suarez <retroguias47@gmail.com>
2023-05-24 16:59:02 -0500

    Animacion de movimiento añadida

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,25 +7,36 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed=1f;
     public float collisionOffset=0.05f;//cuando se hace publica sale en el editor de unity
     public ContactFilter2D movementFilter;
+    public Animator animator;
     Vector2 movementInput;
     Rigidbody2D rb;
     List<RaycastHit2D> castCollisions=new List<RaycastHit2D>();
+    bool success;
     void Start()
     {//busca el componente rigidbody y lo guarda en el atributo
         rb=GetComponent<Rigidbody2D>();
+        animator=GetComponent<Animator>();
         Debug.Log("holo");
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {           if(movementInput==(Vector2.zero)){
+            animator.SetBool("IsMoving",false);      
+        }
+        else{
+            animator.SetBool("IsMoving",true);  
+        }
+        animator.SetFloat("horizontal",movementInput.x);
+        animator.SetFloat("vertical",movementInput.y);
+
+         
     }
     private void FixedUpdate() {
         //si el movimiento no es 0 intentar moverse
         if(movementInput !=Vector2.zero){
-            bool success = TryMove(movementInput);
-
+            success = TryMove(movementInput);
+                 
         }
     }
     private bool TryMove(Vector2 direction){
